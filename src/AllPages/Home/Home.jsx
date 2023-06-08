e17dfd0e9e477a932a48bc4cd8dee78307b53899
ashraf56 <ashrafulfahim07@gmail.com>
2023-06-08 16:56:51 +0600

    About section  completed

--- src/AllPages/Home/Home.jsx
+++ src/AllPages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../HomeSlider/Banner';
+import About from '../About/About';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
+           <About></About>
         </div>
     );
 };
